c26ba801c39d2714dfead7693c684272ec8d53cd
Piyush Kumar <piyushkumar150406@gmail.com>
2026-07-12 17:46:25 +0530

    Time: 1 ms (99.97%), Space: 77 MB (82.56%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,17 +5,12 @@ class Solution {
        int maxsum=Integer.MIN_VALUE;
        for(int i=0;i<n;i++)
        {  
-          if(n==1){
-            return nums[i];
-          }
-          else{
           crrsum=crrsum+nums[i];
           maxsum=Math.max(crrsum,maxsum);
           if(crrsum<0)
           {
             crrsum=0;
           }
-        } 
        }
 
        return maxsum;
