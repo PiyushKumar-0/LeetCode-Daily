393c1c7b5369652ed3361ebd4439f444bf680843
Piyush Kumar <piyushkumar150406@gmail.com>
2026-09-08 21:26:09 +0530

    Time: 1 ms (83.65%), Space: 42.2 MB (97.52%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int count=0;
-        if(n<1000){
-            return 0;
-        }
-        for(int nums=1000;nums<=n;nums++){
-            count+=1;
-        }
-        return count;
+        return n<1000 ? 0:n-999;
     }
 }
